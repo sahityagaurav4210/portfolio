f44e52edacc8a6af16c87a262079d087cfb7e890
Gaurav Sahitya <sahityagaurav.41125@gmail.com>
2025-04-16 19:08:20 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,6 @@ import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 import Projects from '../components/Projects';
-import Skills from '../components/Skills';
 import Loader from '../components/Loader';
 import Contact from '../components/Contact';
 import Support from '../components/Support';
