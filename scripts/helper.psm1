7222ac14e11f1c6937aa925de43bda0e58f6705f
Gaurav Sahitya <gauravsahitya.rockymountaintech@gmail.com>
2025-05-05 13:04:07 +0530

    fix: deployment setup

---
+++ scripts/helper.psm1
@@ -0,0 +1,24 @@
+function Convert-SecureStringToPlainText {
+    param (
+        [System.Security.SecureString]$SecureString
+    )
+  
+    $ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode($SecureString)
+    try {
+        [System.Runtime.InteropServices.Marshal]::PtrToStringUni($ptr)
+    }
+    finally {
+        [System.Runtime.InteropServices.Marshal]::ZeroFreeGlobalAllocUnicode($ptr)
+    }
+}
+  
+function CheckCmdStatus {
+    param ([string]$Msg)
+  
+    if ($LASTEXITCODE -ne 0) {
+        Write-Output "$Msg"
+        exit -1;
+    }
+}
+  
+Export-ModuleMember -Function Convert-SecureStringToPlainText, CheckCmdStatus
--- scripts/windows/deploy.ps1
+++ scripts/windows/deploy.ps1
@@ -1,8 +1,11 @@
-$credentialPath = "credentials.json"
+Import-Module "$PSScriptRoot\..\helper.psm1"
+
+
+$credentialPath = "$PSScriptRoot\..\..\credentials.json"
 $dockerUsername = ""
 $branch = ""
 
-$loginStatus = docker login | grep "Login Succeeded"
+$loginStatus = docker login | findstr "Login Succeeded"
 
 if ($loginStatus -ne "Login Succeeded") {
   $branch = Read-Host "Enter the branch name"
@@ -21,11 +24,16 @@ if (Test-Path -Path $credentialPath) {
   
   Write-Output "Preparing the image..."
   docker build -t $($credentials.ImgName) .
-  docker push $($credentials.ImgName)
+  CheckCmdStatus -Msg "Build failed, exiting..."
 
+  docker push $($credentials.ImgName)
+  CheckCmdStatus -Msg "Build upload failed, exiting..."
+
+  Write-Output "================================="
   Write-Output "Deploying the app, please wait..."
 
   caprover deploy -h "$($credentials.Host)" -p "$($credentials.Password)" -i "$($credentials.ImgName)" --appName "$($credentials.AppName)"
+  CheckCmdStatus -Msg "Deploy failed, exiting..."
 }
 else {
   Write-Output "Preparing the image..."
@@ -34,11 +42,10 @@ else {
     $uri = Read-Host "Enter your caprover host"
     $hashedPwd = Read-Host "Enter your caprover password" -AsSecureString
     $appName = Read-Host "Enter your app name" 
-    $plainPwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-      [Runtime.InteropServices.Marshal]::SecureStringToBSTR($hashedPwd)
-    )
+    $plainPwd = Convert-SecureStringToPlainText -SecureString $hashedPwd
 
     caprover deploy -h "$uri" -p "$plainPwd" --appName "$appName" --branch "$branch" 
+    CheckCmdStatus -Msg "Deploy failed, exiting..."
 
     $fileContents = @{
       Host     = $uri
@@ -59,14 +66,16 @@ else {
   $imgName = Read-Host "Enter your docker image name (without docker username)"
 
   docker build -t "$dockerUsername/$imgName" .
+  CheckCmdStatus -Msg "Build failed, exiting..."
+
   docker push "$dockerUsername/$imgName"
+  CheckCmdStatus -Msg "Build upload, exiting..."
   
   Write-Output "Deploying the app, please wait..."
-  $plainPwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($hashedPwd)
-  )
+  $plainPwd = $plainPwd = Convert-SecureStringToPlainText -SecureString $hashedPwd
   
   caprover deploy -h "$uri" -p "$plainPwd" -i "$dockerUsername/$imgName" --appName "$appName"
+  CheckCmdStatus -Msg "Deploy failed, exiting..."
 
   $fileContents = @{
     Host     = $uri
